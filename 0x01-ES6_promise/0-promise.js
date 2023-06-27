03e00184a110930168899d3615c1625b2b815694
EmmanuelEmp <emmyochogwu@gmail.com>
2023-06-27 22:34:22 +0100

    Promise and error

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,17 +1,4 @@
-function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    // Simulating an asynchronous API call
-    setTimeout(() => {
-      const success = false; // Simulating a failed API call
-      if (success) {
-        const error = new Error('API request failed');
-        reject(error); // Rejecting the Promise with the error
-      } else {
-        const mockResponse = { data: 'Mock response from API' };
-        resolve(mockResponse); // Resolving the Promise with the mock response
-      }
-    }, 2000); // Simulating a delay of 2000 milliseconds (2 seconds)
-  });
+/* eslint-disable */
+export default function getResponseFromAPI() {
+    return new Promise(() => {});
 }
-
-export default getResponseFromAPI;
