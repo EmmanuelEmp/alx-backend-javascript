c1cd34e9cd928f10ee38fd72af2c796755154e40
Ochogwu Emmanuel <87908445+EmmanuelEmp@users.noreply.github.com>
2023-06-27 23:03:21 +0100

    Update 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,16 @@
-export default function iterateThroughObject(reportWithIterator) {
-  return reportWithIterator.join(' | ');
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // Make API request or perform necessary asynchronous operations
+    // Once the operations are completed, call either resolve or reject
+
+    // Example: Simulating an API response after a timeout of 1 second
+    setTimeout(() => {
+      const data = {
+        status: 200,
+        body: 'Success',
+      };
+      resolve(data);
+    }, 1000);
+  });
 }
+
